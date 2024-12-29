396d843634fcb65b1bd4c5f3c2ab2326fe4a17bc
necrokochou <togami.playz2905@gmail.com>
2024-12-30 03:27:09 +0800

    this one the true commit retry

--- src/main/java/net/necrokochou/testmod/TestModDataGenerator.java
+++ src/main/java/net/necrokochou/testmod/TestModDataGenerator.java
@@ -2,10 +2,13 @@ package net.necrokochou.testmod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.necrokochou.testmod.datagen.ModBlockTagProvider;
 
 public class TestModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModBlockTagProvider::new);
 	}
 }
